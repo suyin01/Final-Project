efe98cdef93b662623466554fb7e887a040b9b07
suyin01 <suyinlim@Suyins-MacBook-Pro.local>
2016-03-14 21:11:26 +1100

    changes 14 March

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$('.power-image').hover(function() {
+		$(this).attr('src', 'images/power-2.png');
+//		$(this).toggle(slow);
+	});
+
+	$('.yin-image').hover(function(){
+		$(this).attr('src', 'images/yin-2.png');
+	});
+	
+	$('.basics-image').hover(function(){
+		$(this).attr('src', 'images/basics-2.png');
+	});
+	
+	$('.hot-image').hover(function(){
+		$(this).attr('src', 'images/hot-2.png');
+	});
+	
+	
+});
+//
+//var myName = 'Suyin';
+//	
+//var nameOfFunction = function(somethingIWantToUseInTheFunction) {
+//	alert(somethingIWantToUseInTheFunction);
+//};
+//
+//nameOfFunction(myName);
+//	
+//	
+//	var table = function(var legs = soething that you told me) {
+//	var legs = soething that you told me
+//	console.log(legs);
+//}
+//
+//
+//
+//table(4)
+//
+//function(var legs = 4) {
+//	var legs = 4
+//	alert(legs) // alert 4
+//}
