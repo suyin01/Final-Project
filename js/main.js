f2f7b0ec2e9d13090a4cd195f7f9d1ae0ce3809a
suyin01 <suyinlim@Suyins-MacBook-Pro.local>
2016-03-16 20:53:45 +1100

    edits

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 $(document).ready(function() {
+	
+// when user hovers over 'power' image, power-2 image appears//
 	$('.power-image').hover(function() {
 		$(this).attr('src', 'images/power-2.png');
-//		$(this).toggle(slow);
+	}, function() {
+		$(this).attr('src', 'images/power.png');
 	});
-
+	
+// when user hovers over 'yin' image, yin-2 image appears//
 	$('.yin-image').hover(function(){
 		$(this).attr('src', 'images/yin-2.png');
+	}, function() {
+		$(this).attr('src', 'images/yin.png');
 	});
 	
+// when user hovers over 'basics' image, basics-2 image appears//	
 	$('.basics-image').hover(function(){
 		$(this).attr('src', 'images/basics-2.png');
+	}, function() {
+		$(this).attr('src', 'images/basics.png');
 	});
 	
+// when user hovers over 'hot' image, hot-2 image appears//	
 	$('.hot-image').hover(function(){
 		$(this).attr('src', 'images/hot-2.png');
+	}, function() {
+		$(this).attr('src', 'images/hot.png');
 	});
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 });
