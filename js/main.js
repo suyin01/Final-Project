08ac471098cb07a1c19f29a16aaf9b088d3782c1
suyin01 <suyinlim@Suyins-MacBook-Pro.local>
2016-03-22 07:59:07 +1100

    22 Mar edits

--- js/main.js
+++ js/main.js
@@ -1,45 +1,51 @@
-$(document).ready(function() {
-	
-// when user hovers over 'power' image, power-2 image appears//
-	$('.power-image').hover(function() {
+$(document).ready(function () {
+
+	// when user hovers over 'power' image, power-2 image appears//
+	$('.power-image').hover(function () {
 		$(this).attr('src', 'images/power-2.png');
-	}, function() {
+	}, function () {
 		$(this).attr('src', 'images/power.png');
-	});
+		
+		
+//		// on 'classes' page, the images are static and don't animate//	
+//	if (this).attr('src', '../images/power-2.png');
+//}, function () {
+//	$(this).attr();
 	
-// when user hovers over 'yin' image, yin-2 image appears//
-	$('.yin-image').hover(function(){
+		
+	});
+
+	// when user hovers over 'yin' image, yin-2 image appears//
+	$('.yin-image').hover(function () {
 		$(this).attr('src', 'images/yin-2.png');
-	}, function() {
+	}, function () {
 		$(this).attr('src', 'images/yin.png');
 	});
-	
-// when user hovers over 'basics' image, basics-2 image appears//	
-	$('.basics-image').hover(function(){
+
+	// when user hovers over 'basics' image, basics-2 image appears//	
+	$('.basics-image').hover(function () {
 		$(this).attr('src', 'images/basics-2.png');
-	}, function() {
+	}, function () {
 		$(this).attr('src', 'images/basics.png');
 	});
+
+	// when user hovers over 'hot' image, hot-2 image appears//	
+	$('.hot-image').hover(function () {
+		$(this).attr('src', 'images/hot-2.png');
+	}, function () {
+		$(this).attr('src', 'images/hot.png');
+	});
+
+
 	
-// when user hovers over 'hot' image, hot-2 image appears//	
-	$('.hot-image').hover(function(){
-//		$(this).fadeTo(300, function() {
-			$(this).attr('src', 'images/hot-2.png');
-//			$(this).fadeIn(300);
-//		});
-	}, function() {
-//		$(this).fadeOut(300, function() {
-			$(this).attr('src', 'images/hot.png');
-//			$(this).fadeIn(300);
-		});
-	
-	
-//when user clicks on hamburger button, nav menu drops down//
-	
-	$('.hamburger').click(function(){
+
+
+	//when user clicks on hamburger button, nav menu drops down//
+
+	$('.hamburger').click(function () {
 		$(this).next().slideToggle();
 	});
-	
-	
-	
+
+
+
 });
